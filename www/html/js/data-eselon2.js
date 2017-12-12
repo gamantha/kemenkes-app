0ac27c7859568d4feaba66251f8cd6cd489613ca
Nirwan Praesta <nirwana@xl.co.id>
2017-12-12 09:11:40 +0700

    puter komptensi

--- www/html/js/data-eselon2.js
+++ www/html/js/data-eselon2.js
@@ -218,7 +218,7 @@ var chart = AmCharts.makeChart( "chartdiv", {
    
 	  "startDuration": 0.2,
   "graphs": [ {
-	"balloonText": "<b>[[value]]<b> <br/> Kompetensi : <b>[[x]]</b> | Potensi : <b>[[y]]</b>",	  
+	"balloonText": "<b>[[value]]<b> <br/> Potensi : <b>[[x]]</b> | Kompetensi : <b>[[y]]</b>",	   
     "bullet": "bubble",
          "bulletBorderAlpha": 0.2,
     "bulletBorderThickness": 1,
